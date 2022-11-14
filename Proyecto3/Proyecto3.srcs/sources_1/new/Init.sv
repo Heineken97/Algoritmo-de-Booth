a4ff9610642676930bf327a6604810716ee047aa
Joseph Jimenez <josephdjz@hotmail.com>
2022-11-14 12:11:46 -0600

    [UPDATE]Señales

--- Proyecto3/Proyecto3.srcs/sources_1/new/Init.sv
+++ Proyecto3/Proyecto3.srcs/sources_1/new/Init.sv
@@ -19,11 +19,16 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 interface ControlSignals;
-logic enable;
-endinterface 
+    logic load_A;
+    logic load_B;
+    logic load_add;
+    logic shift_HQ_LQ_Q_1;
+    logic add_sub;
+endinterface
 
 module Init(input pushButton,reset ,logic[7:0]a,logic[7:0]b,logic[3:0]button,output logic[7:0]out, logic [3:0]anode, logic [7:0]cathode );
     ControlSignals control();
+    logic enable;
     logic mult_control;
     logic clk ;
     logic refreshclock;
@@ -31,19 +36,39 @@ module Init(input pushButton,reset ,logic[7:0]a,logic[7:0]b,logic[3:0]button,out
     logic [15:0] Y;
     logic [1:0] refresh_counter;
     logic [3:0] ONE_DIGIT;
-    logic [7:0] oa,ob;
+    logic [7:0] oa;
+    logic [7:0] ob;
+
+    assign control.load_A = 1'b0;
+    assign control.load_B = 1'b0;
+    assign control.load_add = 1'b0;
+    assign control.shift_HQ_LQ_Q_1 = 1'b0;
+    assign control.add_sub = 1'b0;
+    assign enable = 1'b0;
     
-    assign control.enable = 1'b0;
-    Lectura L0(pushButton,a,b,control.enable,oa,ob);
-    
+    always@(enable)
     begin
-        Multiplicador Multiplicador(clk,reset,a,b,mult_control,Q_LSB,Y);
-        ClockDivider Refreshclock_generator(.clk(clk),.divided_clk(refreshclock));
-        RefreshCounter Refreshcounter_wrapper(.refresh_clock(refreshclock),.refresh_counter(refresh_counter));
-        AnodoControl Anode_Control_wrapper(.refresh_counter(refresh_counter),.anode(anode));
-        BCDControl BCD_Control_wrapper(.digit1(out[3:0]),.digit2(out[7:4]),.digit3(button[3:0]),.digit4(button[3:0]),.refresh_counter(refresh_counter),.ONE_DIGIT(ONE_DIGIT));
-        BCDCathode BCD_to_Cathodes_Wrapper(.digit(ONE_DIGIT),.cathode(cathode));
+        case(enable)
+            4'b0000:control.load_A <= 1'b1;
+            4'b0001:control.load_B <= 1'b1;
+            4'b0010:control.load_add <= 1'b1;
+            4'b0011:control.shift_HQ_LQ_Q_1 <= 1'b1;
+            4'b0100:control.add_sub <= 1'b1;
+        endcase
     end
 
+    Lectura L0(pushButton,a,b,enable,oa,ob);
+
+    Multiplicador Multiplicador(clk,reset,a,b,mult_control,Q_LSB,Y);
+
+    ClockDivider Refreshclock_generator(.clk(clk),.divided_clk(refreshclock));
+
+    RefreshCounter Refreshcounter_wrapper(.refresh_clock(refreshclock),.refresh_counter(refresh_counter));
+
+    AnodoControl Anode_Control_wrapper(.refresh_counter(refresh_counter),.anode(anode));
+
+    BCDControl BCD_Control_wrapper(.digit1(out[3:0]),.digit2(out[7:4]),.digit3(button[3:0]),.digit4(button[3:0]),.refresh_counter(refresh_counter),.ONE_DIGIT(ONE_DIGIT));
+
+    BCDCathode BCD_to_Cathodes_Wrapper(.digit(ONE_DIGIT),.cathode(cathode));
 
 endmodule
